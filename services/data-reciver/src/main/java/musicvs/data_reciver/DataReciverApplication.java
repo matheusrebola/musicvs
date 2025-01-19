7541089a00b3247438e5209b9cdf9c7a815f05bd
Matheus Rebola <m.cavalcantir@outlook.com>
2025-01-19 11:08:38 -0300

    first commit

---
+++ services/data-reciver/src/main/java/musicvs/data_reciver/DataReciverApplication.java
@@ -0,0 +1,13 @@
+package musicvs.data_reciver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataReciverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataReciverApplication.class, args);
+	}
+
+}
---
+++ services/data-reciver/src/test/java/musicvs/data_reciver/DataReciverApplicationTests.java
@@ -0,0 +1,13 @@
+package musicvs.data_reciver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataReciverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
