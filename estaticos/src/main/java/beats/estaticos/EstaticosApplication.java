9277c6d178532e473cb52ad71d206c1921994dca
Matheus Rebola <m.cavalcantir@outlook.com>
2025-05-05 13:06:47 -0300

    feat: atualização dos serviços

---
+++ estaticos/src/main/java/beats/estaticos/EstaticosApplication.java
@@ -0,0 +1,13 @@
+package beats.estaticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstaticosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstaticosApplication.class, args);
+	}
+
+}
