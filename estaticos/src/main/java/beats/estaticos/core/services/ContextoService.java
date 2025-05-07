8ab42c60483e41e0399e71f5c3352f397e64599a
Matheus Rebola <m.cavalcantir@outlook.com>
2025-05-07 12:46:55 -0300

    feat: criação do método salvar

--- estaticos/src/main/java/beats/estaticos/core/services/ContextoService.java
+++ estaticos/src/main/java/beats/estaticos/core/services/ContextoService.java
@@ -2,10 +2,17 @@ package beats.estaticos.core.services;
 
 import org.springframework.stereotype.Service;
 
+import beats.estaticos.core.models.Contexto;
+import beats.estaticos.core.repositories.ContextoRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class ContextoService {
+  private final ContextoRepository repository;
+
+  public Contexto salvar(Contexto c) {
+    return repository.save(c);
+  }
 
 }
