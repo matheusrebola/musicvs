a74936372ebcef6b858ad5d94cf1d4944e8ddc47
Matheus Rebola <m.cavalcantir@outlook.com>
2025-05-07 12:06:26 -0300

    feat: configuração das constantes de serviço e mapeamento + import das anotações e DTOs

--- estaticos/src/main/java/beats/estaticos/core/controllers/ArtistaController.java
+++ estaticos/src/main/java/beats/estaticos/core/controllers/ArtistaController.java
@@ -1,21 +1,33 @@
 package beats.estaticos.core.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import beats.estaticos.core.dtos.ArtistaCreateDTO;
+import beats.estaticos.core.dtos.ArtistaDTO;
+import beats.estaticos.core.mappers.ArtistaMapper;
+import beats.estaticos.core.services.ArtistaService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping(path = "/artistas")
 @RequiredArgsConstructor
 public class ArtistaController {
+  private final ArtistaMapper mapper;
+  private final ArtistaService service;
+  
   @PostMapping
-  public void criar(@RequestBody AlbumCreateDTO dto){
-
+  public void criar(@RequestBody ArtistaCreateDTO dto){
+    
   }
 
   @PutMapping("/{id}")
-  public void atualizar(@RequestBody AlbumDTO dto, @PathVariable String id){
+  public void atualizar(@RequestBody ArtistaDTO dto, @PathVariable String id){
 
   }
 
