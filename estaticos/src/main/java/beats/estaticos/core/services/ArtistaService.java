e3c3b7827c46b31cdba7a961d557ab3fa6233373
Matheus Rebola <m.cavalcantir@outlook.com>
2025-05-07 12:13:13 -0300

    feat: configuração da constante de repository + criação do método salvar

--- estaticos/src/main/java/beats/estaticos/core/services/ArtistaService.java
+++ estaticos/src/main/java/beats/estaticos/core/services/ArtistaService.java
@@ -3,13 +3,16 @@ package beats.estaticos.core.services;
 import org.springframework.stereotype.Service;
 
 import beats.estaticos.core.models.Artista;
+import beats.estaticos.core.repositories.ArtistaRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class ArtistaService {public Artista salvar(Artista a) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'salvar'");
+public class ArtistaService {
+  private final ArtistaRepository repository;
+
+  public Artista salvar(Artista a) {
+    return repository.save(a);
   }
 
 }
