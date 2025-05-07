f14447b2786d37f42d3472bfaf30a528346bf440
Matheus Rebola <m.cavalcantir@outlook.com>
2025-05-07 13:38:33 -0300

    feat: criação dos métodos para mapeamento entidade -> e dto -> entidade

--- estaticos/src/main/java/beats/estaticos/core/mappers/GeneroMapper.java
+++ estaticos/src/main/java/beats/estaticos/core/mappers/GeneroMapper.java
@@ -1,5 +1,24 @@
 package beats.estaticos.core.mappers;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import beats.estaticos.core.dtos.GeneroCreateDTO;
+import beats.estaticos.core.dtos.GeneroDTO;
+import beats.estaticos.core.models.Genero;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class GeneroMapper {
+  private final ModelMapper mapper;
+
+  public Genero map(GeneroCreateDTO g) {
+    return mapper.map(g, Genero.class);
+  }
+
+  public GeneroDTO map(Genero g) {
+    return mapper.map(g, GeneroDTO.class);
+  }
 
 }
