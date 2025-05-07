16a74df65464b6ad4bc77b5f8df37a2cd37b3eeb
Matheus Rebola <m.cavalcantir@outlook.com>
2025-05-07 12:10:42 -0300

    feat: configuração das anotações + criação da constante de mapeamento

--- estaticos/src/main/java/beats/estaticos/core/mappers/ArtistaMapper.java
+++ estaticos/src/main/java/beats/estaticos/core/mappers/ArtistaMapper.java
@@ -1,10 +1,17 @@
 package beats.estaticos.core.mappers;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
 import beats.estaticos.core.dtos.ArtistaCreateDTO;
 import beats.estaticos.core.dtos.ArtistaDTO;
 import beats.estaticos.core.models.Artista;
+import lombok.RequiredArgsConstructor;
 
+@Component
+@RequiredArgsConstructor
 public class ArtistaMapper {
+  private final ModelMapper mapper;
 
   public Artista map(ArtistaCreateDTO dto) {
     // TODO Auto-generated method stub
