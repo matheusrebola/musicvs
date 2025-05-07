5cbbef1ba48471fc49a50be81a314614f6b3fe9c
Matheus Rebola <m.cavalcantir@outlook.com>
2025-05-07 12:43:13 -0300

    feat: criação das constantes e ajuste nos parâmetros que serão recebidos pelos métodos

--- estaticos/src/main/java/beats/estaticos/core/controllers/ContextoController.java
+++ estaticos/src/main/java/beats/estaticos/core/controllers/ContextoController.java
@@ -1,22 +1,33 @@
 package beats.estaticos.core.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import beats.estaticos.core.dtos.ContextoCreateDTO;
+import beats.estaticos.core.dtos.ContextoDTO;
+import beats.estaticos.core.mappers.ContextoMapper;
+import beats.estaticos.core.services.ContextoService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping(path = "/contexto-historico")
 @RequiredArgsConstructor
 public class ContextoController {
+  private final ContextoMapper mapper;
+  private final ContextoService service;
 
   @PostMapping
-  public void criar(@RequestBody AlbumCreateDTO dto){
+  public void criar(@RequestBody ContextoCreateDTO dto){
 
   }
 
   @PutMapping("/{id}")
-  public void atualizar(@RequestBody AlbumDTO dto, @PathVariable String id){
+  public void atualizar(@RequestBody ContextoDTO dto, @PathVariable String id){
 
   }
 
