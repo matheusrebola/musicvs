1fd178209c9c380960c07ca9f5a0fd85f63d02a6
Matheus Rebola <m.cavalcantir@outlook.com>
2025-05-07 12:45:57 -0300

    feat: criação da lógica para o método post + alteração do tipo de retorno para response entity

--- estaticos/src/main/java/beats/estaticos/core/controllers/ContextoController.java
+++ estaticos/src/main/java/beats/estaticos/core/controllers/ContextoController.java
@@ -1,5 +1,7 @@
 package beats.estaticos.core.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import beats.estaticos.core.dtos.ContextoCreateDTO;
 import beats.estaticos.core.dtos.ContextoDTO;
 import beats.estaticos.core.mappers.ContextoMapper;
+import beats.estaticos.core.models.Contexto;
 import beats.estaticos.core.services.ContextoService;
 import lombok.RequiredArgsConstructor;
 
@@ -22,8 +25,11 @@ public class ContextoController {
   private final ContextoService service;
 
   @PostMapping
-  public void criar(@RequestBody ContextoCreateDTO dto){
-
+  public ResponseEntity<ContextoDTO> criar(@RequestBody ContextoCreateDTO dto){
+    Contexto c = mapper.map(dto);
+    Contexto s = service.salvar(c);
+    ContextoDTO r = mapper.map(s);
+    return new ResponseEntity<>(r, HttpStatus.CREATED);
   }
 
   @PutMapping("/{id}")
