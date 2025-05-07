a2fbcdd22e169823115b1c657b36b8aa6808e654
Matheus Rebola <m.cavalcantir@outlook.com>
2025-05-07 13:40:00 -0300

    feat: configuração do método de salvar

--- estaticos/src/main/java/beats/estaticos/core/services/GeneroService.java
+++ estaticos/src/main/java/beats/estaticos/core/services/GeneroService.java
@@ -2,10 +2,17 @@ package beats.estaticos.core.services;
 
 import org.springframework.stereotype.Service;
 
+import beats.estaticos.core.models.Genero;
+import beats.estaticos.core.repositories.GeneroRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class GeneroService {
+  private final GeneroRepository repository;
+
+  public Genero salvar(Genero g) {
+    return repository.save(g);
+  }
 
 }
