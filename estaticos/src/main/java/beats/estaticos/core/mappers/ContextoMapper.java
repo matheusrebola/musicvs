5d20a1bb081aa10366f794b013c7d261566a197e
Matheus Rebola <m.cavalcantir@outlook.com>
2025-05-07 12:48:45 -0300

    feat: configuração das anotações + constante de mapper + método para transformar dto -> entidade e entidade -> dto

--- estaticos/src/main/java/beats/estaticos/core/mappers/ContextoMapper.java
+++ estaticos/src/main/java/beats/estaticos/core/mappers/ContextoMapper.java
@@ -1,5 +1,24 @@
 package beats.estaticos.core.mappers;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import beats.estaticos.core.dtos.ContextoCreateDTO;
+import beats.estaticos.core.dtos.ContextoDTO;
+import beats.estaticos.core.models.Contexto;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class ContextoMapper {
+  private final ModelMapper mapper;
+
+  public Contexto map(ContextoCreateDTO c) {
+    return mapper.map(c, Contexto.class);
+  }
+
+  public ContextoDTO map(Contexto c) {
+    return mapper.map(c, ContextoDTO.class);
+  }
 
 }
