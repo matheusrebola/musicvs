00fbb31b307c6b5435249e5675c8a054ca927c23
Matheus Rebola <m.cavalcantir@outlook.com>
2025-05-07 12:09:53 -0300

    feat: criação dos métodos em suas classes

--- estaticos/src/main/java/beats/estaticos/core/mappers/ArtistaMapper.java
+++ estaticos/src/main/java/beats/estaticos/core/mappers/ArtistaMapper.java
@@ -1,5 +1,19 @@
 package beats.estaticos.core.mappers;
 
+import beats.estaticos.core.dtos.ArtistaCreateDTO;
+import beats.estaticos.core.dtos.ArtistaDTO;
+import beats.estaticos.core.models.Artista;
+
 public class ArtistaMapper {
+
+  public Artista map(ArtistaCreateDTO dto) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'map'");
+  }
+
+  public ArtistaDTO map(Artista s) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'map'");
+  }
 
 }
--- estaticos/src/main/java/beats/estaticos/core/services/ArtistaService.java
+++ estaticos/src/main/java/beats/estaticos/core/services/ArtistaService.java
@@ -2,10 +2,14 @@ package beats.estaticos.core.services;
 
 import org.springframework.stereotype.Service;
 
+import beats.estaticos.core.models.Artista;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class ArtistaService {
+public class ArtistaService {public Artista salvar(Artista a) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'salvar'");
+  }
 
 }
